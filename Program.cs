75f8ea4b398e0db834222943acde52c59cf1bdc9
Mirikz <kirilmitrofanov68@gmail.com>
2022-02-01 14:17:41 +0500

    a&b

--- Program.cs
+++ Program.cs
@@ -2,3 +2,20 @@ Console.WriteLine("Enter a");
 double a = Console.ReadLine();
 Console.WriteLine("Enter b");
 double b = Console.ReadLine();
+Console.WriteLine("'+' for plus, '-' for minus, '*' for multiply, '/' for division");
+string choice = Console.ReadLine();
+switch (choice){
+	case "+":
+		plus();
+		break;
+	case "-":
+		minus();
+		break;
+	case "*":
+		multi();
+		break;
+	case "/":
+		division();
+		break;
+}
+	
