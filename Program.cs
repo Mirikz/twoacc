2c39c87432601b626a0749146105abe2e823b765
Mirikq <kiryshka.mitrofanov@gmail.com>
2022-02-01 14:35:26 +0500

    test

---
+++ Program.cs
@@ -0,0 +1,26 @@
+Console.WriteLine("Enter a");
+double a = Console.ReadLine();
+Console.WriteLine("Enter b");
+double b = Console.ReadLine();
+Console.WriteLine("'+' for plus, '-' for minus, '*' for multiply, '/' for division");
+string choice = Console.ReadLine();
+switch (choice){
+	case "+":
+		plus();
+		break;
+	case "-":
+		minus();
+		break;
+	case "*":
+		multi();
+		break;
+	case "/":
+		division();
+		break;
+}
+void plus(double a, double b){
+Console.WriteLine(a+b);
+}
+void minus(double a, double b){
+Console.WriteLine(a-b);
+}
